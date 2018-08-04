69489f439d8a284911fc6728c870f93bf1893051
Bram Ladestein <bram@webelephant.nl>
2018-08-05 01:13:21 +0200

    Fix API Routes

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/users'), 'UserController'
-
-Route::resource('/cruds', 'CrudsController', [
-	'except' => ['edit', 'show', 'store']
-]);
+Route::resource('/users', 'UserController');
